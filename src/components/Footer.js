12c7effe446027e6d1a6d89f59a8e295570f4918
Don Yu <37088038+devzons@users.noreply.github.com>
2023-03-31 14:34:02 -0700

    fix styling issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import { Link, NavLink } from 'react-router-dom'
 
 export const Footer = () => {
   return (
-    <footer className="bg-white shadow m-4 dark:bg-gray-800">
+    <footer className="bg-white shadow dark:bg-gray-900">
       <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023{' '}
@@ -13,22 +13,22 @@ export const Footer = () => {
         </span>
         <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
           <li>
-            <NavLink to="#" target="_blank" className="mr-4 hover:underline md:mr-6 ">
+            <NavLink to="https://instagram.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6 ">
               Instagram
             </NavLink>
           </li>
           <li>
-            <NavLink to="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+            <NavLink to="https://linkedin.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">
               LinkedIn
             </NavLink>
           </li>
           <li>
-            <NavLink to="#" target="_blank" className="mr-4 hover:underline md:mr-6">
+            <NavLink to="https://youtube.com" target="_blank" rel="noreferrer" className="mr-4 hover:underline md:mr-6">
               Youtube
             </NavLink>
           </li>
           <li>
-            <NavLink to="#" target="_blank" className="hover:underline">
+            <NavLink to="https://github.com/devzons" target="_blank" rel="noreferrer" className="hover:underline">
               Github
             </NavLink>
           </li>
